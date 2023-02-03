a9206237a440df48ab8d6576c5a814088fdb6dcd
RushikeshPtl <rushikesh.patil@anveshak.com>
2023-02-03 17:52:01 +0530

    Updated lambda tf

--- modules/lambda_docker/lambda_docker.tf
+++ modules/lambda_docker/lambda_docker.tf
@@ -26,19 +26,20 @@ resource "aws_ecr_repository" "repo" {
 }
  
 resource "null_resource" "ecr_image" {
- triggers = {
-   python_file = md5(file("${local.lambda_source_path}/app.py"))
-   docker_file = md5(file("${local.lambda_source_path}/DockerFile"))
- }
+  triggers = {
+    python_file = md5(file("${local.lambda_source_path}/app.py"))
+    docker_file = md5(file("${local.lambda_source_path}/Dockerfile"))
+  }
  
-provisioner "local-exec" {
-   command = <<EOF
-          aws ecr get-login-password --region ${local.region} | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${local.region}.amazonaws.com
-          cd ${local.lambda_source_path}
-          docker build -t ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag} .
-          docker push ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag}
-       EOF
- }
+  provisioner "local-exec" {
+    command = <<EOF
+            aws ecr get-login-password --region ${local.region} | docker login --username AWS --password-stdin ${local.account_id}.dkr.ecr.${local.region}.amazonaws.com
+            docker build -t ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag} .
+            docker push ${aws_ecr_repository.repo.repository_url}:${local.ecr_image_tag}
+        EOF
+    interpreter = ["pwsh", "-Command"]
+    working_dir = "${local.lambda_source_path}"
+  }
 }
  
 data "aws_ecr_image" "lambda_image" {
